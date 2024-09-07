23c0fccb70c0e20f8c19ded22e0d4be0faf07a82
Nguyen Huy Vu <vunhhe161239@fpt.edu.vn>
2024-09-07 14:02:43 +0700

    d third, right sidebar

--- podcast/components/RightSidebar.tsx
+++ podcast/components/RightSidebar.tsx
@@ -1,6 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const RightSidebar = () => {
+  const router = useRouter();
+
   return <section className="right_sidebar">RightSidebar</section>;
 };
 
