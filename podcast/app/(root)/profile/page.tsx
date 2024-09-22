0cb46ff5288999d8c5063a9ad7d05b6c2de0f774
Nguyen Huy Vu <vunhhe161239@fpt.edu.vn>
2024-09-22 19:31:53 +0700

    d last, add profile 2

--- podcast/app/(root)/profile/page.tsx
+++ podcast/app/(root)/profile/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
   return (
@@ -6,8 +6,10 @@ const Profile = () => {
       <h1 className="text-20 font-bold text-white-1 max-md:text-center">
         Podcaster Profile
       </h1>
+      <div className="mt-6 flex flex-col gap-6 max-md:items-center md:flex-row"></div>
+      <section className="mt-9 flex flex-col gap-5"></section>
     </section>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
